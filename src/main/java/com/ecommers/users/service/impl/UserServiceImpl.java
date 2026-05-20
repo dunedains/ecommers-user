175e1e918086f79e5c0cee11fd689d5409cc8e9c
dunedains <fe.zapatao@duocuc.cl>
2026-05-20 00:22:14 -0400

    add logging

--- src/main/java/com/ecommers/users/service/impl/UserServiceImpl.java
+++ src/main/java/com/ecommers/users/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommers.users.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import com.ecommers.users.dto.UserDto;
 import com.ecommers.users.exception.UserNotFoundException;
 import com.ecommers.users.model.User;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -20,17 +22,20 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto.UserResponse getUserById(Long id) {
+        log.info("Buscando usuario id={}", id);
         return toResponse(findOrThrow(id));
     }
 
     @Override
     public List<UserDto.UserResponse> getAllUsers() {
+        log.info("Listando todos los usuarios");
         return repository.findAll().stream().map(this::toResponse).toList();
     }
 
     @Override
     @Transactional
     public UserDto.UserResponse createUser(UserDto.UserRequest request) {
+        log.info("Creando usuario email={}", request.email());
         User user = new User();
         user.setName(request.name());
         user.setEmail(request.email());
@@ -41,6 +46,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public UserDto.UserResponse updateUser(Long id, UserDto.UserRequest request) {
+        log.info("Actualizando usuario id={}", id);
         User user = findOrThrow(id);
         user.setName(request.name());
         user.setEmail(request.email());
@@ -51,6 +57,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void deleteUser(Long id) {
+        log.info("Eliminando usuario id={}", id);
         if (!repository.existsById(id)) throw new UserNotFoundException(id);
         repository.deleteById(id);
     }
